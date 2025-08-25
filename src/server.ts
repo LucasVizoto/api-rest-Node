718f7b723fea9a3cc2efbc705545268c37e5970d
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-08-25 18:34:02 -0300

    config: adding eslint on project

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify = require("fastify")
+import fastify = require('fastify')
 
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
-
-
-
-app.listen({
-    port: 3333
-}).then(() => {
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP Server Running')
-})
+  })
+// ES Lint, seguindo uma padronização de projeto em node
